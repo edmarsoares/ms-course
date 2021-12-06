8d4996991a97b20310d8c3b223e6af16c192d802
edmar <edmar.soares@cwi.com.br>
2021-12-06 20:06:12 -0300

    adicionado hystrix fallback

--- hr-payroll/src/main/java/com/payroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/payroll/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.payroll.model.Payment;
 import com.payroll.services.PaymentService;
 
@@ -17,9 +18,16 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
+		
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days) {
+		Payment payment = new Payment("Edmar", 500.0, days);
 		
 		return ResponseEntity.ok(payment);
 	}
