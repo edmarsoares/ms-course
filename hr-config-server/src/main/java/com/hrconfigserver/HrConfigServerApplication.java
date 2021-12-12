ce6016dcc6ade7e264f53c27ec46a49f0189ee5d
edmar <edmar.soares@cwi.com.br>
2021-12-12 12:40:03 -0300

    add private access to github

--- hr-config-server/src/main/java/com/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,19 @@
 package com.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication{
+	
+	@Value("${spring.cloud.config.server.git.password}")
+	private String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
-
 }
