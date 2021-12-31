f940a33be19a64d81baa4b9fa6835abe34ffaa87
edmar <edmar.soares@cwi.com.br>
2021-12-31 09:50:04 -0300

    oauth and Jwt

--- hr-oauth/src/main/java/com/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/com/hroauth/service/UserService.java
@@ -3,13 +3,16 @@ package com.hroauth.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.hroauth.feignclient.UserFeignClient;
 import com.hroauth.model.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	@Autowired
@@ -23,6 +26,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("email found: " , email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if (user == null) {
+			logger.error("email not found: " , username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("email found: " , username);
 		return user;
 	}
 }
